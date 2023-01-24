23ec67a2be0f9d90ab3e60a9fe745651e7a9e1be
EvEnzyme <Mj20011020@outlook.com>
2023-01-24 16:28:19 -0500

    Update Plane.py

--- GameObjects/Plane.py
+++ GameObjects/Plane.py
@@ -1,17 +1,23 @@
 import pygame
 from random import randint
 from pygame.sprite import Sprite
-from setting import PLANE_SPEED, PLANE_SIZE, PLANE_POS
+from setting import PLANE_SPEED, PLANE_SIZE, PLANE_POS, PLANE_OSCILLATION_SPEED
+from math import sin
 
 class Plane(Sprite):
     def __init__(self):
         super().__init__()
         self.image = pygame.image.load('graphics/obstacle/plane.png').convert_alpha()
         x, y = PLANE_POS
-        y = PLANE_POS[1] + randint(0,260)
+        self.y = PLANE_POS[1] + randint(0,260)
         self.rect = self.image.get_rect(topleft = (x,y))
+
+        self.animation_index = 0
     
     def update(self):
         self.rect.x -= PLANE_SPEED
         if self.rect.x <= -900:
             self.kill()
+
+        self.rect.y = self.y+int(25*sin(self.animation_index))
+        self.animation_index += PLANE_OSCILLATION_SPEED
